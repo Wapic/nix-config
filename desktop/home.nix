f19d62d4ce86de1751cffe2aed6ca18900a19365
Wapic <17051068+Wapic@users.noreply.github.com>
2024-11-11 20:23:45 +0100

    setup desktop environment

--- desktop/home.nix
+++ desktop/home.nix
@@ -1,21 +1,41 @@
-{ lib, config, pkgs, ... }: {
-    home = {
-        stateVersion = "24.05";
+{ config, pkgs, nix-colors, ... }: {
+    imports = [
+        ../modules/home-manager
+        nix-colors.homeManagerModules.default
+    ];
 
-        pointerCursor = {
-            gtk.enable = true;
-            package = pkgs.adwaita-icon-theme;
-            name = "Adwaita";
-            size = 30;
-        };
+    home.stateVersion = "24.05";
 
-        packages = with pkgs; [
-            typescript-language-server
-            bash-language-server
-            lua-language-server
-            pyright
-            lavanda-gtk-theme
-            gnome-themes-extra
+    programs = {
+        spotify.enable = false;
+    };
+
+    services = {
+        hypridle.settings.listener = [
+            {
+                timeout = 300;
+                on-timeout = "brightnessctl -s set 10";
+                on-resume = "brightnessctl -r";
+            }
+            {
+                timeout = 900;
+                on-timeout = "hyprctl dispatch dpms off";
+                on-resume = "hyprctl dispatch dpms on";
+            }
         ];
     };
+
+    wayland.windowManager.hyprland.settings = {
+        monitor = [
+            "desc:Acer Technologies XV272U KV 91121D1DA4203,preferred,0x0,auto"
+            "desc:AOC AG276QZD 1322131231233,preferred,2560x0,auto"
+            "desc:Ancor Communications Inc ASUS MG278 0x0001F437,preferred,5120x0,auto"
+        ];
+
+        device = {
+            name = "endgame-gear-endgame-gear-op1-8k-gaming-mouse";
+            sensitivity = 0.0;
+        };
+    };
 }
+

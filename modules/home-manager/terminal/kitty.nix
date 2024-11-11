9cd7875129c8e52f564731d37d700dd4af517c64
Wapic <17051068+Wapic@users.noreply.github.com>
2024-11-11 21:56:15 +0100

    enable kitty zsh integration

--- modules/home-manager/terminal/kitty.nix
+++ modules/home-manager/terminal/kitty.nix
@@ -2,6 +2,7 @@
 
     programs.kitty = {
         enable = true;
+        shellIntegration.enableZshIntegration = true;
         settings = {
             background_opacity = 1;
             enable_audio_bell = "no";

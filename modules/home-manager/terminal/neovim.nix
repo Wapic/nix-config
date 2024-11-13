38e14b3fd81dd8d8bacbbb022c448fa811f1d0ab
Wapic <17051068+Wapic@users.noreply.github.com>
2024-11-13 13:09:59 +0100

    enable all aliases for neovim

--- modules/home-manager/terminal/neovim.nix
+++ modules/home-manager/terminal/neovim.nix
@@ -9,7 +9,9 @@
     programs.neovim = {
         enable = true;
         defaultEditor = true;
+        viAlias = true;
         vimAlias = true;
+        vimdiffAlias = true;
 
         plugins = with pkgs.vimPlugins; [
             fzf-vim
